1287d0b3b3cd3d948a9c700cb6efe19ebd4922b3
DragonCloudBurst <katieaspivey@gmail.com>
2025-05-25 23:10:40 -0400

    update maint.sh

--- maint.sh
+++ maint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# basically, just update everything.
+
+# general packages
 sudo apt update
 sudo apt upgrade
+sudo apt-get update
+flatpak update
+
+# lang-specific packages
 pipx upgrade-all
+sudo npm update -g
+rustup update
+sudo update_rubygems
